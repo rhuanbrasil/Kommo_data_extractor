9557d1d731d5d80100b54d8752c3df198c40a6e0
Rhuan <rhuanbrazil@gmail.com>
2026-05-07 15:42:05 -0300

    feat :arrow_up: : Resposta em formato de DataFrame -Transformei as respostas da requisição em json -Utilizei o pandas para formatação da saída e estabelecimento de colunas padrão

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import requests
 import os
+import pandas as pd
 from dotenv import load_dotenv
 from Utils import Util
 load_dotenv()
@@ -15,10 +15,26 @@ if os.path.exists("tokens.json") == False:
 
 tokens_dict = Util.get_tokens()   
 
-leads = Util.get_leads(tokens_dict['access_token'], subdomain)
+response = Util.get_leads(tokens_dict['access_token'], subdomain)
+leads = response.json()
 
-if leads.status_code == 401:
+if response.status_code == 401:
     print("Token expirou, gerando outro...")
-    Util.write_tokens_from_refresh(client_id, client_secret, tokens_dict["refresh_token"], url)  
+    Util.write_tokens_from_refresh(client_id, client_secret, tokens_dict["refresh_token"], url)
+    response = Util.get_leads(tokens_dict['access_token'], subdomain)
+    leads = response.json()
+ 
+df = pd.DataFrame(leads["_embedded"]["leads"])
 
-print(leads.json())
+df['tags'] = ([", ".join([tag.get("name") for tag in lead.get("_embedded", {}).get("tags", [])])
+               for lead in leads.get("_embedded").get("leads")
+            ])
+
+
+df = df[['id', 'responsible_user_id', 'pipeline_id', 'created_at', 'tags']]
+
+print (df.head(10))
+
+#Se quiser o resultado em csv
+#df.to_csv("Leads.csv", index=False, encoding="utf-8")
+
